f1c6199c9ed47c9b771be31bcc13c85a924f913b
hoaison2204 <Hoaison2204@gmail.com>
2020-09-15 08:35:32 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const myMath = Request('./myModule.js');
+
+console.log(myMath.double(3));
+console.log(myMath.reactangleArena(5,7));
---
+++ myModule.js
@@ -0,0 +1,10 @@
+var mathLib ={
+    double : function(x){
+        return x*2;
+    },
+
+    rectangleArena : function (width, height){
+        return (width + height);
+    }
+};
+module.exports = mathLib;
